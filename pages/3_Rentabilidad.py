9fe0f597d9f0bdc18f464a7f3e5cae7f355f0f52
rentas-camacho-dev <bookingcamacho92@gmail.com>
2026-09-23 16:54:30 -0500

    Update 3_Rentabilidad.py

--- pages/3_Rentabilidad.py
+++ pages/3_Rentabilidad.py
@@ -1242,7 +1242,7 @@ with top1:
 with top2:
 
     if st.button(
-        "⌂\nPortafolio",
+        "⌂  Portafolio",
         key="nav_portafolio",
         use_container_width=True,
         help="Portafolio"
@@ -1259,7 +1259,7 @@ with top2:
 with top3:
 
     if st.button(
-        "▥\nPropiedades",
+        "▥  Propiedades",
         key="nav_propiedades",
         use_container_width=True,
         help="Propiedades"
@@ -1276,7 +1276,7 @@ with top3:
 with top4:
 
     if st.button(
-        "▤\nOcupación",
+        "▤  Ocupación",
         key="nav_ocupacion",
         use_container_width=True,
         help="Ocupación"
@@ -1293,7 +1293,7 @@ with top4:
 with top5:
 
     if st.button(
-        "$\nFinanciero",
+        "$  Financiero",
         key="nav_financiero",
         use_container_width=True,
         help="Financiero"
@@ -1310,7 +1310,7 @@ with top5:
 with top6:
 
     if st.button(
-        "⌁\nAnálisis",
+        "↗  Análisis",
         key="nav_analisis",
         use_container_width=True,
         help="Análisis"
@@ -1327,7 +1327,7 @@ with top6:
 with top7:
 
     if st.button(
-        "▧\nReportes",
+        "▧  Reportes",
         key="nav_reportes",
         use_container_width=True,
         help="Reportes"
